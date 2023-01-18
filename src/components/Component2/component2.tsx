d19d252af3958d29214e6f706ee4770db0ba053c
Houzair Koussa <houzairmk@icloud.com>
2023-01-18 22:54:01 +0400

    [chore] REFAC *

---
+++ src/components/Component2/component2.tsx
@@ -0,0 +1,7 @@
+import { component$ } from '@builder.io/qwik';
+
+export const Component2 = component$(() => {
+  return (
+    <></>
+  );
+});
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
-export { Logo } from './components/logo/logo';
-export { Counter } from './components/counter/counter';
+export { Component2 } from './components/Component2/component2';
+export { Component1 } from './components/component1/component1';
