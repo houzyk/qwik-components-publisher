a114c363d8559340f3096b7cc4737fd89b2b349a
Houzair Koussa <houzairmk@icloud.com>
2023-01-21 10:35:59 +0400

    [dev] UPDATE storybook preview with rendering and loader for qwik

--- .storybook/preview.cjs
+++ .storybook/preview.cjs
@@ -1,9 +1,13 @@
-export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
+import { QWIK_LOADER } from '@builder.io/qwik/loader/index';
+import { render } from '@builder.io/qwik';
+
+eval(QWIK_LOADER);
+
+export const decorators = [
+  (Story) => {
+    const parent = document.createElement('div');
+    const jsxNode = Story();
+    render(parent, jsxNode);
+    return parent;
   },
-}
+];
