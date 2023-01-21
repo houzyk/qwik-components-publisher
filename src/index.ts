646527e9776c4b268ff0fc70f4ce405d20829d36
Houzair Koussa <88334281+houzyk@users.noreply.github.com>
2023-01-21 10:45:43 +0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
 export { Component2 } from './components/Component2/component2';
-export { Component1 } from './components/Component1/component1';
+export { Component1 } from './components/component1/component1';
