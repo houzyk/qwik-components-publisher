03abfc363c08aab90973c1e72894bb4f4681778a
Houzair Koussa <houzairmk@icloud.com>
2023-01-21 10:35:17 +0400

    [dev] UPDATE storybook config for transpiling vite -> html

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,18 +1,23 @@
+const vite = require('vite');
+const viteTsconfig = require('vite-tsconfig-paths');
+
+const tsconfigPaths = viteTsconfig.default;
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/html",
-  "core": {
-    "builder": "@storybook/builder-vite"
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+  framework: '@storybook/html',
+  core: {
+    builder: '@storybook/builder-vite',
   },
-  "features": {
-    "storyStoreV7": true
-  }
-}
+  viteFinal: async (config, options) => {
+    const { qwikVite: qwikVite } = await import('@builder.io/qwik/optimizer');
+    config.plugins?.unshift(qwikVite());
+    return vite.mergeConfig(config, {
+      plugins: [tsconfigPaths()],
+    });
+  },
+  features: {
+    storyStoreV7: true,
+  },
+};
